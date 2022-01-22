abd208532cb8ef996a17b33852a37b153eab2ee1
John Connor <john@retinize.com>
2022-01-22 13:13:58 +0000

    Movie Texture and Trigger Scripts

---
+++ Assets/TriggerActivateButtonPress.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerActivateButtonPress : TriggerActivate
+{
+    private bool inTriggerZone;
+
+    public static Action<string> inInteractiveTriggerZone;
+    public static Action leavingInteractiveTriggerZone;
+    protected override void ActivateTriggerEffect()
+    {
+        inTriggerZone = true;
+        inInteractiveTriggerZone?.Invoke("Press Space Bar");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            inTriggerZone = false;
+            leavingInteractiveTriggerZone?.Invoke();
+        }
+    }
+
+    private void Update()
+    {
+        if(!inTriggerZone) return;
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            base.ActivateTriggerEffect();
+        }
+    }
+}
